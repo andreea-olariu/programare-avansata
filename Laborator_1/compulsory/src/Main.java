a606a6aa4d22ae94eec67e3006acbcab6e9d7e60
andreea-olariu <83142073+andreea-olariu@users.noreply.github.com>
2023-02-20 14:44:48 +0200

    lab_1-compulsory

---
+++ Laborator_1/compulsory/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n = (int) (Math.random() * 1_000_000);
+        n *= 3;
+        n += 0b10101;
+        n += 0xFF;
+        n *= 6;
+
+        while(n >= 10) {
+            int sum = 0;
+            while(n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        System.out.println(n);
+        System.out.println("Willy-nilly, this semester I will learn " + languages[n]);
+    }
+}
